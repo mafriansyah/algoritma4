7d5d4bcf02afbdae8d323f079c608447fdd611b1
mafriansyah <ryanksp34@gmail.com>
2024-08-31 11:30:54 +0700

    04 Perulangan Foreach

---
+++ 04 Perulangan Foreach.php
@@ -0,0 +1,17 @@
+<?php
+$books = [
+    "Pahri",
+    "Harimau",
+    "Hafidz",
+    "Buaya"
+];
+
+echo "<h5>Nama Hewan:</h5>";
+echo "<ul>";
+foreach($books as $buku){
+    echo "<li>$buku</li>";
+}
+echo "</ul>";
+?>
+
+print_r("<p><strong>by :M Afriansyah");

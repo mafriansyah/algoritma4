2e3c67da2c64697c0c37f220fd6e1f924dc6da97
mafriansyah <ryanksp34@gmail.com>
2024-09-02 12:50:15 +0700

    28 break pada Switch

---
+++ 28 break pada Switch.php
@@ -0,0 +1,19 @@
+<?php
+$buah = "jeruk";
+
+switch ($buah) {
+    case "mangga":
+        echo "Ini mangga";
+        break;
+    case "jeruk":
+        echo "Ini jeruk";
+        // Break disini menghentikan switch
+        break;
+    case "pir":
+        echo "Ini pir";
+        break;
+    default:
+        echo "Buah tidak dikenal";
+}
+
+echo "<p><strong><i>By M AFRIANSYAH</strong>";

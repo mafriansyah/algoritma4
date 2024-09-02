47165b85d309b9294ff5fc3b5b3f573c538459bc
mafriansyah <ryanksp34@gmail.com>
2024-09-02 12:36:28 +0700

    24 Foreach Loop dengan Key

---
+++ 24 Foreach Loop dengan Key.php
@@ -0,0 +1,8 @@
+<?php
+$harga = array("mangga" => 10000, "jeruk" => 5000, "pir" => 15000);
+
+foreach ($harga as $kunci => $nilai) {
+    echo "Harga dari " . $kunci . " adalah " . $nilai . "<br>";
+}
+
+echo "<p><strong><i>By M AFRIANSYAH</strong>";

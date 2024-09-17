20f5772681d396068c17d175ed56c830a1c3e659
mafriansyah <ryanksp34@gmail.com>
2024-09-17 10:07:21 +0700

    38 mengasosiasikan sebuah kata kunci

---
+++ 38 mengasosiasikan sebuah kata kunci .php
@@ -0,0 +1,10 @@
+<?php
+$email["subjek"] = "Apa Kabar?";
+$email["pengirim"] = "ryanksp34@gmail.com";
+$email["isi"] = "Apa kabar Rian? sudah lama kita tidak berjumpa";
+
+echo "<pre>";
+print_r($email);
+echo "</pre>";
+
+echo "<p><strong><i>By M Afriansyah</strong>";
